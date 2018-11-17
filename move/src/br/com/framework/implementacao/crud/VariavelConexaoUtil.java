fbc4d11325a7cd275321e250b23fa3bedf72bfa7
MagnanimoT <45125843+MagnanimoT@users.noreply.github.com>
2018-11-17 13:40:58 -0200

    Uppload do move
    primeiro comit

---
+++ move/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * Nome do caminho do JNDI darasource tomcat aula 2 HibernateUtil
+ * @author Rockstead
+ *
+ */
+
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+}
